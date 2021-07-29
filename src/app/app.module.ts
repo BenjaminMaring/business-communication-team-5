1c0cc1c0e8aa2e0370c63915590400608d8218cd
Invis <MaringBen@hotmail.com>
2021-07-28 21:41:15 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomePageComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
